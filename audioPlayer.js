0ecd8fa4d0a9fb3d324ed295aadf105229261512
Khushi Kharke <khushi.kharke111@gmail.com>
2023-06-17 04:38:28 +0530

    hamburger menu navbar

---
+++ audioPlayer.js
@@ -0,0 +1,13 @@
+import lottieWeb from "https://cdn.skypack.dev/lottie-web";
+const playIconContainer = document.getElementById('play-icon');
+const audioPlayerContainer = document.getElementById('audio-player-container');
+const sleekSlider = document.getElementById('seek-slider');
+const volumeSlider = document.getElementById('volume-slider');
+const muteIconContainer = document.getElementById('mute-icon');
+let playState = 'play';
+let muteState = 'unmute';
+ const playAnimation = lottieWeb.loadAnimation({
+    
+ })
+
+
